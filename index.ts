4453f7c60605edcf0a60eef6f9f93264c55dd907
Francis Pham <francispham89@gmail.com>
2021-05-17 10:58:33 -0700

    Introduced Basic Functions

--- index.ts
+++ index.ts
@@ -21,3 +21,22 @@ const notGoodPracticeII: any = 2;
 
 let never: never;
 
+
+/* Functions in TS */
+const typedFunction = (word: string): string => {
+  console.log(word);
+  return word;
+};
+// typedFunction(myName);
+
+const optionalParamsFunction = (word?: string): string => {
+  console.log(word || 'Phong');
+  return word || 'Phong';
+}
+// optionalParamsFunction();
+
+const defaultParamsFunction = (word = 'Default', ...otherStuff: string[]): string => {
+  console.log(otherStuff);
+  return word;
+}
+// defaultParamsFunction('Francis', 'Pham');

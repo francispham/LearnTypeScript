487cd96cf081df4c5dbcd787547efa29c6161c90
Francis Pham <francispham89@gmail.com>
2021-05-16 17:44:05 -0700

    Introduced Basic Types

--- index.ts
+++ index.ts
@@ -1,1 +1,23 @@
-console.log('Learn TypeScript NOW');
+/* Basic Types: */ 
+const isOpen: boolean = false;
+
+const myName: string = 'Francis';
+
+const myAge: number = 32.50;
+
+const list: number[] = [0, 1, 2];
+
+const me: [string, number, boolean] = ['Francis', 32, false];
+
+enum Job { 
+  WebDev,
+  DevOps, 
+  BlockchainDev 
+};
+const job: Job = Job.WebDev;
+
+const notGoodPracticeI: any = 'NoGood';
+const notGoodPracticeII: any = 2;
+
+let never: never;
+

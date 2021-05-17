286e4e34b5c962dbc283a1ad8465e376de80a2a6
Francis Pham <francispham89@gmail.com>
2021-05-17 12:08:12 -0700

    Introduced Implicit Types, Union Types & Null Types

--- index.ts
+++ index.ts
@@ -1,3 +1,5 @@
+/* 1. TYPES */
+
 /* Basic Types: */ 
 const isOpen: boolean = false;
 
@@ -40,3 +42,30 @@ const defaultParamsFunction = (word = 'Default', ...otherStuff: string[]): strin
   return word;
 }
 // defaultParamsFunction('Francis', 'Pham');
+
+
+/* Implicit Types & Union Types with | */
+let implicitI: string | number | boolean = 'implicit';
+implicitI = 'newImplicit';
+implicitI = true;   //  Invalid without Union Type included '| boolean'
+implicitI = 10;     //  Invalid without Union Type included '| number'
+// console.log('implicitI:', implicitI);
+
+// Gets Type from Initial Declaration:
+let implicitII = implicitI;
+implicitII = 100;   //  Invalid if Type is not a Number because the last Type of implicitI is Number  
+
+const unionType = (x: string | number): string => {
+  return `margin: ${x}px;`;
+};
+// unionType(10);
+// unionType('10');
+// unionType(false);    // Invalid
+
+
+/* Null Types */ 
+let nullType: string | undefined = 'Null Type';
+nullType = null;  // null is included in String Type
+// console.log('nullType:', nullType);
+nullType = 'NullII';
+nullType = undefined; // undefined is Invalid but No Error Showed
